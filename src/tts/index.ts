9810037f59f82a1e9115513ef5164645e4410228
StudyMate AI <studymate@ai.com>
2026-06-17 19:50:02 +0000

    Module 5: Text-to-Speech - TTSService, TTSPlayer, SentenceHighlighter, TTSSettings

---
+++ src/tts/index.ts
@@ -0,0 +1,5 @@
+export { default as TTSService } from './TTSService';
+export type { TTSVoice, TTSPreferences } from './TTSService';
+export { default as TTSPlayerComponent } from './TTSPlayerComponent';
+export { default as SentenceHighlighter } from './SentenceHighlighter';
+export { default as TTSSettingsComponent } from './TTSSettingsComponent';
